8a0790d671c622360548592df72fa74d829f92b3
VolantTyler <33227593+VolantTyler@users.noreply.github.com>
2018-07-09 16:57:19 -0400

    feat: Add 'have URLs' and 'have names' tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,12 +31,23 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        it('have URLs', function() {
+            allFeeds.forEach(url => {
+                expect(url).toBeDefined();
+                expect(url.length).not.toBe(0);
+            });
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('have names', function() {
+            allFeeds.forEach(name => {
+                expect(name).toBeDefined();
+                expect(name.length).not.toBe(0);
+            });
+        });
     });
 
 
